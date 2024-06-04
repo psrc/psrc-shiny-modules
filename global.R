a1b859144f6e0e3df4e2668315ef442ba16cd0d7
Craig Helmann <chelmann@psrc.org>
2024-06-04 09:25:37 -0700

    working app skeleton

---
+++ global.R
@@ -0,0 +1,54 @@
+# Packages ----------------------------------------------------------------
+
+# Packages for Interactive Web application
+library(shiny)
+library(shinyBS)
+library(shinydashboard)
+library(bs4Dash)
+library(shinycssloaders)
+library(bslib)
+
+# Packages for Data Cleaning/Processing
+library(tidyverse)
+
+# Packages for Chart Creation
+library(psrcplot)
+library(echarts4r)
+
+# Packages for Map Creation
+library(sf)
+library(leaflet)
+
+# Packages for Table Creation
+library(DT)
+
+# Package for Excel Data Creation
+library(openxlsx)
+
+# Run Modules Files ---------------------------------------------------------------------------
+# This section runs the modules and unless the folder name changes, it doesn't need to be changed
+# It also loads in useful functions for dashboard creation
+module_files <- list.files('modules', full.names = TRUE)
+sapply(module_files, source)
+source("functions.R")
+
+# Page Information --------------------------------------------------------
+# This section reads in the csv files that provide the text used on the relevant pages
+# Unless the file names change, it doesn't need to be changed
+left_panel_info <- read_csv("data/left_panel_information.csv", show_col_types = FALSE)
+page_text <- read_csv("data/page_text.csv", show_col_types = FALSE)
+
+# Inputs ---------------------------------------------------------------
+# Section for any standard inputs like the crs for wgs84
+wgs84 <- 4326
+load_clr <- "#91268F"
+latest_yr <- "2023"
+
+# Data via RDS files ------------------------------------------------------
+# Section reading in inputs from the data folder. RDS files are R objects and read quickly
+ntd_data <- readRDS("data/ntd_data.rds")
+
+# Values for Drop Downs ---------------------------------------------------
+# Section for creating the values needed in any dropdowns, lists, etc.
+ntd_metric_list <- as.character(unique(ntd_data$metric))
+ntd_mode_list <- ntd_data |> select("variable") |> filter(variable != "All Transit Modes") |> distinct() |> pull()
---
+++ ui.R
@@ -0,0 +1,44 @@
+shinyUI(
+
+  fluidPage(
+    
+    tags$style(HTML("
+    .tabbable > .nav > li > a {background-color: #005753;  color:white}
+    .tabbable > .nav > li.active > a {background-color: #91268F; color:white}
+  ")),
+    
+    id = "AppID", # Shiny App ID
+    tags$style("@import url(https://use.fontawesome.com/releases/v6.3.0/css/all.css);"),
+    title = "My Shiny App", # Internal Shiny title
+    
+    theme = "styles.css", # Loads in the custom CSS
+    
+    # This section adds the PSRC logo on the top left of the page and the Page Title
+    fluidRow(column(4, tags$a(div(tags$img(src='psrc-logo.png',
+                                           style="margin-top: 10px; padding-left: 40px;",
+                                           height = "80")
+                                  ), href="https://www.psrc.org", target="_blank")),
+             column(8, br(), strong(tags$div(class="mainpage_title", "My App is Really Cool")))),
+    
+    hr(style = "border-top: 1px solid #000000;"),
+    
+    # This is a page type that uses a sidebar
+    sidebarLayout(
+      
+      # Left Panel of the page using the left panel module
+      sidebarPanel(left_panel_ui('leftMain')),
+      
+      mainPanel(
+        fluidRow(column(12, style='padding-left:25px; padding-right:50px;',
+                        tabsetPanel(type = "pills",
+                                    tabPanel("Overview", transit_overview_ui('OVERVIEWtransit')),
+                                    tabPanel("Region", transit_region_ui('REGIONtransit')),
+                                    tabPanel("Mode", transit_mode_ui('MODEtransit')))
+                        )) # end of fluid row for Transit tab
+        ) # End of mainPanel
+    ), # End of sidebarLayout
+    
+    tags$footer(footer_ui('psrcfooter'))
+  
+    ) # End of fluid page
+) # end of shiny app
